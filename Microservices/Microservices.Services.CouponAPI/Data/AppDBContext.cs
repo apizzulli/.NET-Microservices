be0b42eaf0b16c0c629ee9c7018c8a0e0e026d05
Anthony Pizzulli <anthony.pizzulli1@gmail.com>
2023-11-21 14:00:43 -0500

    working on DB connection

---
+++ Microservices/Microservices.Services.CouponAPI/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microservices.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Microservices.Services.CouponAPI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Microservices/Microservices.Services.CouponAPI/Program.cs
+++ Microservices/Microservices.Services.CouponAPI/Program.cs
@@ -1,6 +1,15 @@
+using Microservices.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDBContext>(option =>
+{
+    option.UseSqlServer("");
+
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
